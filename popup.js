87ef379eb53e0c7a6155f9b091c41089e07e9780
hskim95 <novicedev95@gmail.com>
2025-12-13 00:09:50 +0900

    complete main functions
    To Do: refactoring checkbox interaction mechanism

--- popup.js
+++ popup.js
@@ -15,6 +15,18 @@ const FileExtensions = Object.freeze([
     ".cmp",
     ".tst"
 ]);
+
+const DependencyTable = Object.freeze({
+    1: [0],
+    2: [0],
+    3: [0],
+    4: [1],
+    5: [0, 2],
+    6: [1],
+    7: [3],
+    8: [3]
+});
+
 // 8-bit Comp(0b1)
 const optionAllProject =
 (1<<(ProjectNumber.PROJECT_MAX - ProjectNumber.PROJECT_MIN + 1)) - 1;
@@ -41,7 +53,7 @@ document.querySelector(
     'input#extension-all[type="checkbox"][data-origin="static"]');
 
 const backupCombobox =
-document.querySelector('select#backupCollection[data-origin="static"]');
+document.querySelector('select#backupCombobox[data-origin="static"]');
 
 // ===== HTML Element (injected) =====
 let unitProjectList;
@@ -127,6 +139,28 @@ function addExtensionCheckbox() {
     });
 }
 
+function addProjectExtensionDependency() {
+    unitProjectList.forEach(checkbox => {
+        checkbox.addEventListener("change", () => {
+            const dependencySet = new Set();
+            let currentProjectBit = collectOptions().project;
+            for (let i = 0; i < unitProjectList.length; i++) {
+                const projectNumber = i + 1;
+                if (currentProjectBit & 1) {
+                    DependencyTable[projectNumber].forEach(value => dependencySet.add(value));
+                }
+                currentProjectBit >>>= 1;
+                if (currentProjectBit === 0) break;
+            }
+            for (let i = 0; i < unitExtensionList.length - 2; i++) {
+                const fixedIndex = i;
+                unitExtensionList[fixedIndex].disabled =
+                !dependencySet.has(fixedIndex);
+            }
+        });
+    });
+}
+
 function collectOptions() {
     const projectSize = ProjectNumber.PROJECT_MAX - ProjectNumber.PROJECT_MIN + 1;
     let projectOptionBit = 0;
@@ -155,7 +189,7 @@ function injectDocumentFragment(htmlString, parent) {
 
 function makeComboboxHTMLString(stringArray) {
     const partialString = [];
-    partialString.push('<option value="" disabled selected hidden>선택하세요...</option>');
+    partialString.push('<option value="" disabled selected hidden>...</option>');
     for (let i = 0; i < stringArray.length; i++) {
         partialString.push('<option value="' + stringArray[i] +'">' + stringArray[i] +"</option>");
     }
@@ -163,10 +197,59 @@ function makeComboboxHTMLString(stringArray) {
     return htmlString;
 }
 
+function addLocalizedMessage() {
+    const captionElement = document.querySelector("caption");
+    captionElement.textContent = chrome.i18n.getMessage("caption");
+
+    const buttonList = document.querySelectorAll('button[id$=_Btn][data-origin="static"]');
+    const buttonArray = Array.from(buttonList);
+    buttonArray[0].textContent = chrome.i18n.getMessage("buttonName1");
+    buttonArray[1].textContent = chrome.i18n.getMessage("buttonName2");
+    buttonArray[2].textContent = chrome.i18n.getMessage("buttonName3");
+
+    const buttonDescriptionList = document.querySelectorAll(
+        'td[id^=buttonDescription][data-origin="static"]');
+    const buttonDescriptionArray = Array.from(buttonDescriptionList);
+    buttonDescriptionArray[0].textContent = chrome.i18n.getMessage(
+        "buttonDescription1");
+    buttonDescriptionArray[1].textContent = chrome.i18n.getMessage(
+        "buttonDescription2");
+    buttonDescriptionArray[2].textContent = chrome.i18n.getMessage(
+        "buttonDescription3");
+
+    const legendList = document.querySelectorAll(
+        'legend[id$="-legend"][data-origin="static"]');
+    const legendArray = Array.from(legendList);
+    legendArray[0].textContent = chrome.i18n.getMessage("localization_Project");
+    legendArray[1].textContent = chrome.i18n.getMessage("localization_FileExtension");
+
+    const labelList = document.querySelectorAll('label[id$="-label"][data-origin="static"]');
+    const labelArray = Array.from(labelList);
+
+    console.log("labelArray[0]: " + labelArray[0].textContent);
+    console.log("labelArray[1]: " + labelArray[1].textContent);
+    console.log("labelArray[2]: " + labelArray[2].textContent);
+
+    labelArray[0].firstChild.nodeValue = "[" +
+    chrome.i18n.getMessage("localization_All");
+    labelArray[1].firstChild.nodeValue = "[" +
+    chrome.i18n.getMessage("localization_All");
+    /* labelArray[2].textContent = labelArray[2].textContent.replace(
+        "__MSG_comboboxDescription__", chrome.i18n.getMessage("comboboxDescription"));
+        */
+    labelArray[2].textContent = chrome.i18n.getMessage("comboboxDescription");
+}
+
+async function onDOMLoaded() {
+    addProjectCheckbox();
+    addExtensionCheckbox();
+    addProjectExtensionDependency();
+    addLocalizedMessage();
+    await updateBackupList();
+}
+
 // ===== Initialize =====
-document.addEventListener("DOMContentLoaded", updateBackupList);
-document.addEventListener("DOMContentLoaded", addProjectCheckbox);
-document.addEventListener("DOMContentLoaded", addExtensionCheckbox);
+document.addEventListener("DOMContentLoaded", onDOMLoaded);
 
 backupAllButtonElement.addEventListener("click", async () => {
     // To Do: read all local storage data(key:value pairs)
@@ -181,9 +264,9 @@ backupAllButtonElement.addEventListener("click", async () => {
 
     chrome.notifications.create({
         iconUrl: "Extension128.png",
-        message: "Result: " + response.status +
-        "\nSaved name: " + response.message,
-        title: "Job finished! (Backup All)",
+        title: chrome.i18n.getMessage("notificationTitle1"),
+        message: chrome.i18n.getMessage("notificationResultHeading1") + response.status +
+        "\n" + chrome.i18n.getMessage("notificationResultHeading2") + response.message,
         type: "basic"
     });
     if (response.status === "ok") {
@@ -197,8 +280,20 @@ backupAllButtonElement.addEventListener("click", async () => {
 backupSelectionButtonElement.addEventListener("click", async () => {
     // vvvvv Remove on Release!!! vvvvv
     console.log("[Debug] send message (backup, selection): popup.js");
+    // ^^^^^ Remove on Release!!! ^^^^^
 
     const options = collectOptions();
+
+    if (options.project * options.extension === 0) {
+        chrome.notifications.create({
+            iconUrl: "Extension128.png",
+            title: chrome.i18n.getMessage("refuseTitle"),
+            message: chrome.i18n.getMessage("refuseDetail"),
+            type: "basic"
+        });
+        return;
+    }
+
     const response = await chrome.runtime.sendMessage({action: "backup", option: options });
 
     // vvvvv Remove on Release!!! vvvvv
@@ -207,8 +302,8 @@ backupSelectionButtonElement.addEventListener("click", async () => {
 
     chrome.notifications.create({
         iconUrl: "Extension128.png",
-        message: "Result: " + response.status,
-        title: "Job finished! (Backup Selected)",
+        title: chrome.i18n.getMessage("notificationTitle2"),
+        message: chrome.i18n.getMessage("notificationResultHeading1") + response.status + "\n" + chrome.i18n.getMessage("notificationResultHeading2") + response.message,
         type: "basic"
     });
     if (response.status === "ok") {
@@ -226,14 +321,14 @@ loadButtonElement.addEventListener("click", () => {
                 const response = await chrome.runtime.sendMessage({ action: "load to browser", backupName: choice });
                 chrome.notifications.create({
                     iconUrl: "Extension128.png",
-                    message: "Result: " + response.status,
-                    title: "Backup data loaded.",
+                    title: chrome.i18n.getMessage("notificationTitle3"),
+                    message: chrome.i18n.getMessage("notificationResultHeading1") + response.status,
                     type: "basic"
                 });
             })();
         }
         else {
-            console.warn("[Warning] Try loading with no selected data.");
+            console.warn("[Warning] Try loading without any selected data.");
         }
         return true;
 });
